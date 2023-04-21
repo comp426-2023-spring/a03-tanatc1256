245db7a28c1d8d7e424cb672ddb320ec699554b1
root <root@PremsLaptop.localdomain>
2023-04-21 14:12:36 -0400

    fix import statment

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { rps } from '../../lib/rpsls.js';
+import { rps } from '../lib/rpsls.js';
 import minimist from 'minimist';
 
 const args = minimist(process.argv.slice(2));
