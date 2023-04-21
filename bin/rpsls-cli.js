041de0f298fd3a338c373aaf1c01c344f1e23f14
root <root@PremsLaptop.localdomain>
2023-04-21 14:06:23 -0400

    fix import statement

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/evn node
 
-import { rpsls } from './lib/rpsls.js';
+import { rpsls } from '../../lib/rpsls.js';
 import minimist from 'minimist';
 
 const args = minimist(process.argv.slice(2));
